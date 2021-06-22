713e02dd2925833edcf147ec4f66e6db74a03928
denilson.souza <denilson.souza.o@gmail.com>
2021-06-22 19:58:59 -0300

    Day 2 - Update

--- src/controllers/CreateUsersController.ts
+++ src/controllers/CreateUsersController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express"
-import { RepositoryNotTreeError } from "typeorm";
 import { CreateUserService } from "../service/CreateUserService";
 
 class CreateUsersController {
