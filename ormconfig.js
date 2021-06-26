bbcb5548159c87269796561808eecaff424420c2
denilson.souza <denilson.souza.o@gmail.com>
2021-06-26 09:57:47 -0300

    Fix ssl conection

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,9 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  synchronize: true,
-  dropSchema: false,
-  logging: true,
-  ssl: {
-    ca: process.env.SSL_CERT,
+  "ssl": true,
+  "extra": {
+    "ssl": { "rejectUnauthorized": false }
   },
   "migrations": ["dist/database/migrations/*.ts"],
   "entities": ["dist/entities/*.ts"],
